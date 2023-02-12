0448ab0916f87e02a491df640fa59c21ea76a842
Saba <saba.sadegh67@gmail.com>
2023-02-12 10:19:35 +0330

    editing part 2

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="filter-nav">
+    <button @click="updateFilter('all')" :class="{ active: current === 'all' }">
+      View All
+    </button>
+    <button
+      @click="updateFilter('complete')"
+      :class="{ active: current === 'complete' }"
+    >
+      Complete
+    </button>
+    <button
+      @click="updateFilter('ongoing')"
+      :class="{ active: current === 'ongoing' }"
+    >
+      Ongoing
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["current"],
+  methods: {
+    updateFilter(by) {
+      this.$emit("filterchange", by);
+    },
+  },
+};
+</script>
+
+<style>
+.filter-nav {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav @filterChang="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProject" :key="project.id">
         <SingleProject
           :project="project"
           @delete="handleDelete"
@@ -42,6 +42,17 @@ export default {
         return project.id === id;
       });
       p.complete = !p.complete;
+    },
+  },
+  computed: {
+    filteredProject() {
+      if (this.current === "compeleted") {
+        return this.projects.filter((project) => project.complete);
+      }
+      if (this.current === "ongoing") {
+        return this.projects.filter((project) => !project.complete);
+      }
+      return this.projects;
     },
   },
 };
